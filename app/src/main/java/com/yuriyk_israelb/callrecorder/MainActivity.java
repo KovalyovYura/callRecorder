d4fc29e4a123257bef621c745565307e4c7bb440
Yura Kovalyov <kovalyovyur@gmail.com>
2019-12-31 13:24:03 +0200

    first commit

---
+++ app/src/main/java/com/yuriyk_israelb/callrecorder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yuriyk_israelb.callrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
